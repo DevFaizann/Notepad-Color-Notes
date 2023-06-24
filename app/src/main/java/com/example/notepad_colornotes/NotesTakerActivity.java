4a0dd8085c10ca132616c14716147037170e9ab5
DevFaizann <faizanmansuri.work@gmail.com>
2023-06-24 21:15:06 +0530

    Completed the project

---
+++ app/src/main/java/com/example/notepad_colornotes/NotesTakerActivity.java
@@ -0,0 +1,14 @@
+package com.example.notepad_colornotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesTakerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_taker);
+    }
+}
